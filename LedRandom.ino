7b78c20e5ae9e36dc064765d11a25915808d422d
sandrey <sandrey84@mail.ru>
2018-03-18 17:26:06 +0200

    added led on time adjustment

--- LedRandom.ino
+++ LedRandom.ino
@@ -14,14 +14,13 @@ const int UPPERCUT_PROBABILITY_PIN = A1;
 const int FRONT_PROBABILITY_PIN = A2;
 const int HOOK_PROBABILITY_PIN = A3;
 const int LEFT_RIGHT_RESISTOR_PIN = A4;
+const int LED_ON_PERIOD_RESISTOR_PIN = A5;
 
 //app constants
 const int LED_ON = 255;
 const int LED_OFF = 0;
-const int LED_ON_MILLIS = 100;
-const int LED_PAUSE_MILLIS = 100;
-const int MIN_INTERVAL = 500;
-const int MAX_INTERVAL = 4000;
+const int MIN_HIT_INTERVAL_MS = 500;
+const int MAX_HIT_INTERVAL_MS = 4000;
 const long INTERVAL_RANDOM_COEFF = 60;//% -> 0.6
 const long SPURT_INTERVAL_COEFF = 70;//% -> 0.7
 const long SPURT_PROBABILITY_COEFF = 5;//% -> 0.5
@@ -31,9 +30,13 @@ const int HIGH_LEFT_HOOK = 2;
 const int HIGH_RIGHT_UPPERCUT = 3;
 const int HIGH_RIGHT_FRONT = 4;
 const int HIGH_RIGHT_HOOK = 5;
+const int ANALOG_MIN = 0;
+const int ANALOG_MAX = 1023;
 const int ANALOG_RANGE = 1024;
 const int ANALOG_MIN_THRESHOLD = 15;
 const int MAX_HITS_IN_SPURT = 10;
+const int LED_ON_MIN_PERIOD_MS = 50;
+const int LED_ON_MAX_PERIOD_MS = 500;
 
 //variables
 long highLeftFrontTurnOnMillis;
@@ -73,28 +76,33 @@ void loop() {
 
 void turnOffWithDelay() {
   long now = millis();
+  int ledOnMillis = map(analogRead(LED_ON_PERIOD_RESISTOR_PIN), 
+                        ANALOG_MIN, 
+                        ANALOG_MAX, 
+                        LED_ON_MIN_PERIOD_MS, 
+                        LED_ON_MAX_PERIOD_MS);
 
-  if(highLeftFrontTurnOnMillis + LED_ON_MILLIS < now) {
+  if(highLeftFrontTurnOnMillis + ledOnMillis < now) {
     analogWrite(HIGH_LEFT_FRONT_LED_PIN, LED_OFF);
   }
   
-  if(highLeftHookTurnOnMillis + LED_ON_MILLIS < now) {
+  if(highLeftHookTurnOnMillis + ledOnMillis < now) {
     analogWrite(HIGH_LEFT_HOOK_LED_PIN, LED_OFF);
   }
 
-  if(highLeftUppercutTurnOnMillis + LED_ON_MILLIS < now) {
+  if(highLeftUppercutTurnOnMillis + ledOnMillis < now) {
     analogWrite(HIGH_LEFT_UPPERCUT_LED_PIN, LED_OFF);
   }
   
-  if(highRightFrontTurnOnMillis + LED_ON_MILLIS < now) {
+  if(highRightFrontTurnOnMillis + ledOnMillis < now) {
     analogWrite(HIGH_RIGHT_FRONT_LED_PIN, LED_OFF);
   }
 
-  if(highRightHookTurnOnMillis + LED_ON_MILLIS < now) {
+  if(highRightHookTurnOnMillis + ledOnMillis < now) {
     analogWrite(HIGH_RIGHT_HOOK_LED_PIN, LED_OFF);
   }
 
-  if(highRightUppercutTurnOnMillis + LED_ON_MILLIS < now) {
+  if(highRightUppercutTurnOnMillis + ledOnMillis < now) {
     analogWrite(HIGH_RIGHT_UPPERCUT_LED_PIN, LED_OFF);
   }
 }
@@ -114,7 +122,7 @@ void turnOn(int led) {
       break;
     case HIGH_LEFT_UPPERCUT:
       analogWrite(HIGH_LEFT_UPPERCUT_LED_PIN, LED_ON);
-      highLeftHookTurnOnMillis = now;
+      highLeftUppercutTurnOnMillis = now;
       break;  
     case HIGH_RIGHT_FRONT:
       analogWrite(HIGH_RIGHT_FRONT_LED_PIN, LED_ON);
@@ -132,7 +140,11 @@ void turnOn(int led) {
 }
 
 int getNextDelay() {
-  long determinedDelay = map(analogRead(SPEED_RESISTOR_PIN), 0, 1023, MIN_INTERVAL, MAX_INTERVAL);
+  long determinedDelay = map(analogRead(SPEED_RESISTOR_PIN), 
+                            ANALOG_MIN, 
+                            ANALOG_MAX, 
+                            MIN_HIT_INTERVAL_MS, 
+                            MAX_HIT_INTERVAL_MS);
   
   return spurtTime
       ? determinedDelay - (determinedDelay * SPURT_INTERVAL_COEFF / 100.0)
